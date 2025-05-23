8cd73e1838b630825b408b3fb09ec4008744759f
phaniapt <118794806+phaniapt@users.noreply.github.com>
2025-05-23 17:20:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-app.get('/', (req, res) => res.send('Hello all welcome to @solugenix-hyderabad'));
+app.get('/', (req, res) => res.send('Hello all welcome to @solugenix-india'));
 app.listen(port, () => console.log(`App running on port ${port}`));
