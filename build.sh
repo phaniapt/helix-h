768b14af6cb19b9b7bb23738bf5ce34eb7da59c0
phaniapt <118794806+phaniapt@users.noreply.github.com>
2025-05-26 12:41:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t devphani/harness_pipeline_docker_image:v3 .
-docker push devphani/harness_pipeline_docker_image:v3
+docker build -t devphani/harness_pipeline_docker_image:v4 .
+docker push devphani/harness_pipeline_docker_image:v4
